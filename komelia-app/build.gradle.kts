5e9bb9aceb1cfa1b770bee29be78b6cd1ad3db8a
MKDevTests <ton@email.com>
2026-06-10 16:55:37 +0200

    chore(release): v1.1.2

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -124,7 +124,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         manifestPlaceholders["appLabel"] = "Kora"
-        versionCode = 10101
+        versionCode = 10102
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
--- komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
+++ komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(1, 1, 1)
+        val current = AppVersion(1, 1, 2)
 
         fun fromString(value: String): AppVersion {
             val sanitized = value.removePrefix("v")
