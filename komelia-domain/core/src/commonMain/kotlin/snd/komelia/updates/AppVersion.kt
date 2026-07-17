18a0594c4a2cfb5bd5b38165658b9ce5dcf00f26
MKDevTests <ton@email.com>
2026-07-17 15:36:12 +0200

    chore(release): v1.1.10

--- komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
+++ komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(1, 1, 9)
+        val current = AppVersion(1, 1, 10)
 
         fun fromString(value: String): AppVersion {
             val sanitized = value.removePrefix("v")
