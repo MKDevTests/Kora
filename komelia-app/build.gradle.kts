ea9516774485d4b87e2676deb098acefad8ff2b4
MKDevTests <ton@email.com>
2026-08-25 19:24:05 +0200

    chore(release): v1.8.5

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -134,7 +134,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         manifestPlaceholders["appLabel"] = "Kora"
-        versionCode = 10804
+        versionCode = 10805
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
