b801c915c3e7b6d8c6b51a61e48552faf72107e9
MKDevTests <ton@email.com>
2026-09-02 11:00:57 +0200

    chore(release): v1.8.11

--- komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
+++ komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(1, 8, 10)
+        val current = AppVersion(1, 8, 11)
 
         fun fromString(value: String): AppVersion =
             fromStringOrNull(value) ?: error("Can't parse version number")
