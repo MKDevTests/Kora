8896aa10cb58d1489657028507bf288d18f9c457
MKDevTests <ton@email.com>
2026-09-03 11:03:20 +0200

    chore(release): v1.8.13

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -134,7 +134,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         manifestPlaceholders["appLabel"] = "Kora"
-        versionCode = 10812
+        versionCode = 10813
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
--- komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
+++ komelia-domain/core/src/commonMain/kotlin/snd/komelia/updates/AppVersion.kt
@@ -17,7 +17,7 @@ data class AppVersion(
 ) : Comparable<AppVersion> {
 
     companion object {
-        val current = AppVersion(1, 8, 12)
+        val current = AppVersion(1, 8, 13)
 
         fun fromString(value: String): AppVersion =
             fromStringOrNull(value) ?: error("Can't parse version number")
